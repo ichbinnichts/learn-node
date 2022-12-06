74236251cbe0eb8e6ab68a9df2e536e7d5a73d70
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-12-06 16:30:35 -0300

    feat: default

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const http = require('http');
-const fs = require('fs');
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
 const server = http.createServer((req, res)=>{
-    fs.readFile('index.html', function(err, data){
-        res.writeHead(200,{'content-type':'text/html'});
-        res.write(data);
-        return res.end();
-    })
+    
 })
 
 server.listen(port, hostname, ()=>{
-    console.log('Server is listening');
+    console.log('Server is listening on port:', port);
 })
